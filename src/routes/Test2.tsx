e1be6605e019324c785a7d599f18dd9f519237b0
Megan <smith.megan.design@gmail.com>
2025-09-07 16:50:28 -0700

    feat: rename test -> index.tsx - james
    Signed-off-by: Megan <smith.megan.design@gmail.com>

--- src/routes/Test2.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/Test2')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/Test2"!</div>
-}
